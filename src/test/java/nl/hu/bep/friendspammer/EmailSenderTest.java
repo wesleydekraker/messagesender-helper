5a3630353f38b9580b589279655da8e04c211169
Wesley de Kraker <github@wesleydekraker.com>
2018-04-25 12:00:28 +0200

    Added failing test class

---
+++ src/test/java/nl/hu/bep/friendspammer/EmailSenderTest.java
@@ -0,0 +1,12 @@
+package nl.hu.bep.friendspammer;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class EmailSenderTest {
+
+  @Test
+  public void test_fail() {
+    fail("Let's test the failing unit test");
+  } 
+}
