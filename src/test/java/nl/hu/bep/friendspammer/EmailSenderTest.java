6ddd5f5263f9b1f49aa4e6651ea93295f132ac3c
Wesley de Kraker <github@wesleydekraker.com>
2018-04-25 12:04:25 +0200

    Fixed failing test

--- src/test/java/nl/hu/bep/friendspammer/EmailSenderTest.java
+++ src/test/java/nl/hu/bep/friendspammer/EmailSenderTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class EmailSenderTest {
 
   @Test
-  public void test_fail() {
-    fail("Let's test the failing unit test");
+  public void testConstructor() {
+    new EmailSender();
   } 
 }
