c686505cb4862d3935530bd45928dcd3b4eaf54e
Wesley de Kraker <github@wesleydekraker.com>
2018-04-28 15:28:06 +0200

    Simplified mongo saver

---
+++ src/main/java/nl/hu/bep/friendspammer/DatabaseFactory.java
@@ -0,0 +1,31 @@
+package nl.hu.bep.friendspammer;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.client.MongoDatabase;
+
+public class DatabaseFactory {
+    private MongoDatabase database;
+    
+    public DatabaseFactory() {
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        
+        @SuppressWarnings("resource")
+        MongoClient mongoClient = new MongoClient("mongo",
+                MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build());
+        
+        database = mongoClient.getDatabase("friendspammer");
+    }
+    
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+
+}
--- src/main/java/nl/hu/bep/friendspammer/MongoSaver.java
+++ src/main/java/nl/hu/bep/friendspammer/MongoSaver.java
@@ -1,86 +1,41 @@
 package nl.hu.bep.friendspammer;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoSaver {
-	static final Logger logger = LoggerFactory.getLogger(MongoSaver.class);
-	
-	private MongoSaver() { }
-	
-	public static boolean saveEmail(String to, String from, String subject, String text, Boolean html) {
-		String database = "friendspammer";
-		
-		boolean success = true;
-		
-		try (MongoClient mongoClient = new MongoClient("mongo") ) {
-			
-			MongoDatabase db = mongoClient.getDatabase( database );
-			
-			MongoCollection<Document> c = db.getCollection("email");
-			
-			Document  doc = new Document ("to", to)
-			        .append("from", from)
-			        .append("subject", subject)
-			        .append("text", text)
-			        .append("asHtml", html);
-			c.insertOne(doc);
-		} catch (MongoException mongoException) {
-			logger.debug("Error while saving to mongo");
-			
-			StringWriter stringWriter = new StringWriter();
-			mongoException.printStackTrace(new PrintWriter(stringWriter));
-			logger.debug(stringWriter.toString());
-			
-			success = false;
-		}
-		
-		return success;
-	}
-	
-	   public static List<Email> getEmails() {
-	       String database = "friendspammer";
+    private static final Logger logger = LoggerFactory.getLogger(MongoSaver.class);
+    private static final DatabaseFactory databaseFactory = new DatabaseFactory();
 
-	       MongoClient mongoClient = new MongoClient("mongo");
-	       
-	       MongoDatabase db = mongoClient.getDatabase(database);
-	       
-	       MongoCollection<Document> c = db.getCollection("email");
-	       
-	       Iterator<Document> it = c.find().iterator();
-	       
-	       List<Email> emails = new ArrayList<>();
-	       
-	       while(it.hasNext())
-	       {
-	           Document doc = it.next();
-	           
-	           Email email = new Email();
-	           email.setTo(doc.getString("to"));
-               email.setFrom(doc.getString("from"));
-               email.setSubject(doc.getString("subject"));
-               email.setMessageBody(doc.getString("text"));
-               email.setAsHtml(doc.getBoolean("asHtml"));
-               
-               emails.add(email);
-	       }
+    private MongoSaver() { }
 
+    public static void saveEmail(Email email) {
+        MongoDatabase database = databaseFactory.getDatabase();
+        
+        MongoCollection<Email> collection = database.getCollection("email", Email.class);
+        
+        collection.insertOne(email);
+    }
 
-	       mongoClient.close();
-	       return emails;
-	    }
-
+    public static List<Email> getEmails() {
+        MongoDatabase database = databaseFactory.getDatabase();
+        
+        MongoCollection<Email> collection = database.getCollection("email", Email.class);
+        
+        Iterator<Email> emailIterator = collection.find().iterator();
+        
+        List<Email> emails = new ArrayList<Email>();
+        while (emailIterator.hasNext()) {
+            emails.add(emailIterator.next());
+        }
+        
+        return emails;
+    }
 }
-
